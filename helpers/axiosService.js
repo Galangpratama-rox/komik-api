378d4dd39b7d3d49f0cdb90dcf6c73d567a57a16
Muhammad Galang <galangm504@gmail.com>
2026-05-05 19:23:33 +0700

    commis

--- helpers/axiosService.js
+++ helpers/axiosService.js
@@ -4,6 +4,9 @@ const axiosCookieJarSupport = require("axios-cookiejar-support").default;
 axiosCookieJarSupport(axios);
 
 axios.defaults.baseURL = baseUrl;
+axios.defaults.headers.common["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+axios.defaults.headers.common["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8";
+axios.defaults.headers.common["Accept-Language"] = "en-US,en;q=0.9";
 
 const AxiosService = async (url) => {
   return new Promise(async (resolve, reject) => {
